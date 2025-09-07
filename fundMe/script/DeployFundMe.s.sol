238cc3aed734beab09499fefd877f3ef427edf32
Vishal772-pixel <fantasydark001@gmail.com>
2025-09-07 15:34:01 +0530

    adding more contracst

--- fundMe/script/DeployFundMe.s.sol
+++ fundMe/script/DeployFundMe.s.sol
@@ -2,19 +2,14 @@
 
 pragma solidity 0.5.11;
 // forge script script/DeployFundMe.s.sol
+
 import {FundMe} from "../src/Fundme.sol";
 import {Script} from "forge-std/src/Script.sol";
 
-
 contract DeployFundMe is Script {
-    function run () external {
+    function run() external {
         vm.startBroadcast();
         new FundMe();
         vm.stopBroadcast();
-        
-    
-    
     }
-    
-    }
-
+}
